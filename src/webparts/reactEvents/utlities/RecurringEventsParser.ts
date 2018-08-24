616097d4245c62c2b1c3124217c5d91c88417848
Tse Kit Yam <TseKi@esquel.com>
2018-08-24 18:15:50 +0800

    tmp

--- src/webparts/reactEvents/utlities/RecurringEventsParser.ts
+++ src/webparts/reactEvents/utlities/RecurringEventsParser.ts
@@ -1,58 +1,58 @@
 import { SPEvent } from "../components/ReactEventsContainer";
 import { RBCEvent } from "../components/IReactEventsProps";
-import * as JXON from "jxon";
+import * as xml2js from "xml2js";
 import * as moment from "moment";
 
 interface IRepeatWeekly {
-  $su?: string; // "True", "False"
-  $mo?: string; // "True", "False"
-  $tu?: string; // "True", "False"
-  $we?: string; // "True", "False"
-  $th?: string; // "True", "False"
-  $fr?: string; // "True", "False"
-  $sa?: string; // "True", "False"
-  $weekFrequency: string; // number string
+  $su?: boolean;
+  $mo?: boolean;
+  $tu?: boolean;
+  $we?: boolean;
+  $th?: boolean;
+  $fr?: boolean;
+  $sa?: boolean;
+  $weekFrequency: number;
 }
 
 interface IRepeatDaily {
-  $weekday?: string; // "True", "False"
-  $dayFrequency?: string; // number string
+  $weekday?: boolean;
+  $dayFrequency?: number;
 }
 
 interface IRepeatMonthly {
-  $day: string; // number string
-  $monthFrequency: string; // number string
+  $day: number;
+  $monthFrequency: number;
 }
 
 interface IRepeatMonthlyByDay {
-  $su?: string; // "True", "False"
-  $mo?: string; // "True", "False"
-  $tu?: string; // "True", "False"
-  $we?: string; // "True", "False"
-  $th?: string; // "True", "False"
-  $fr?: string; // "True", "False"
-  $sa?: string; // "True", "False"
-  $monthFrequency: string; // number
+  $su?: boolean;
+  $mo?: boolean;
+  $tu?: boolean;
+  $we?: boolean;
+  $th?: boolean;
+  $fr?: boolean;
+  $sa?: boolean;
+  $monthFrequency: number;
   $weekdayOfMonth: string; // "first", "second", "third", "fourth", "last"
 }
 
 interface IRepeatYearly {
-  $day: string; // number string
-  $month: string; // number string from 1 to 12
-  $yearFrequency: string;
+  $day: number;
+  $month: number; // from 1 to 12
+  $yearFrequency: number;
 }
 
 interface IRepeatYearlyByDay {
-  $month: string; // number string from 1 to 12
-  $su?: string; // "True", "False"
-  $mo?: string; // "True", "False"
-  $tu?: string; // "True", "False"
-  $we?: string; // "True", "False"
-  $th?: string; // "True", "False"
-  $fr?: string; // "True", "False"
-  $sa?: string; // "True", "False"
+  $month: number; // from 1 to 12
+  $su?: boolean;
+  $mo?: boolean;
+  $tu?: boolean;
+  $we?: boolean;
+  $th?: boolean;
+  $fr?: boolean;
+  $sa?: boolean;
   $weekdayOfMonth: string; // "first", "second", "third", "fourth", "last"
-  $yearFrequency: string; // number
+  $yearFrequency: number;
 }
 
 interface IRepeat {
@@ -69,8 +69,8 @@ interface IRecurrenceData {
     rule: {
       firstDayOfWeek: string;
       repeat: IRepeat;
-      repeatInstances?: string; // number string
-      repeatForever?: string; // "True", "False"
+      repeatInstances?: number;
+      repeatForever?: boolean;
       windowEnd?: string; // date string, i.e. "2018-08-27T05:00:00Z"
     };
   };
@@ -78,64 +78,83 @@ interface IRecurrenceData {
 
 export default class RecurringEventsParser {
   public static parse(event: SPEvent): RBCEvent[] {
-    // console.log(event);
-    let recurrenceData: IRecurrenceData = JXON.stringToJs(event.RecurrenceData);
-    let repeat: IRepeat = recurrenceData.recurrence.rule.repeat;
-    if (repeat.daily) {
-      return this.parseDaily(event);
-    } else if (repeat.monthly) {
-      return this.parseMonthly(event);
-    } else if (repeat.monthlyByDay) {
-    } else if (repeat.yearly) {
-    } else if (repeat.yearlyByDay) {
-    }
+    let options: any = {
+      mergeAttrs: true,
+      explicitArray: false,
+      valueProcessors: [
+        xml2js.processors.parseNumbers,
+        xml2js.processors.parseBooleans
+      ]
+    };
+
+    xml2js.parseString(event.RecurrenceData, options, function(
+      err: any,
+      result: any
+    ) {
+      console.dir(result);
+    });
+
+    // let recurrenceData: IRecurrenceData = xml2js.stringToJs(
+    //   event.RecurrenceData
+    // );
+    // let repeat: IRepeat = recurrenceData.recurrence.rule.repeat;
+    // if (repeat.daily) {
+    //   return this.parseDaily(event);
+    // } else if (repeat.monthly) {
+    //   return this.parseMonthly(event);
+    // } else if (repeat.monthlyByDay) {
+    // } else if (repeat.yearly) {
+    // } else if (repeat.yearlyByDay) {
+    // }
     return [];
   }
 
-  private static parseDaily(value: SPEvent): RBCEvent[] {
-    let recurrenceData: IRecurrenceData = JXON.stringToJs(value.RecurrenceData);
-    let repeat: IRepeat = recurrenceData.recurrence.rule.repeat;
-    let rule = repeat.daily;
+  // private static parseDaily(value: SPEvent): RBCEvent[] {
+  //   let recurrenceData: IRecurrenceData = xml2js.stringToJs(
+  //     value.RecurrenceData
+  //   );
+  //   let repeat: IRepeat = recurrenceData.recurrence.rule.repeat;
+  //   let rule: IRepeatDaily = repeat.daily;
 
-    let events: RBCEvent[] = [];
+  //   let events: RBCEvent[] = [];
 
-    if (rule.$weekday) {
-      for (
-        let m: moment.Moment = moment(value.EventDate);
-        m.isBefore(moment(value.EndDate));
-        m.add(1, "d")
-      ) {
-        let n: moment.Moment = moment(m);
+  //   if (rule.$weekday) {
+  //     for (
+  //       let m: moment.Moment = moment(value.EventDate);
+  //       m.isBefore(moment(value.EndDate));
+  //       m.add(1, "d")
+  //     ) {
+  //       let n: moment.Moment = moment(m);
 
-        if (0 < n.day() && n.day() < 6) {
-          events.push({
-            start: n.toDate(),
-            end: n.add(parseInt(value.Duration), "s").toDate(),
-            title: value.Title,
-            allDay: value.fAllDayEvent === true
-          });
-        }
-      }
-    } else if (rule.$dayFrequency) {
-      for (
-        let m: moment.Moment = moment(value.EventDate);
-        m.isBefore(moment(value.EndDate));
-        m.add(rule.$dayFrequency, "d")
-      ) {
-        let n: moment.Moment = moment(m);
-        events.push({
-          start: n.toDate(),
-          end: n.add(parseInt(value.Duration), "s").toDate(),
-          title: value.Title,
-          allDay: value.fAllDayEvent === true
-        });
-      }
-    }
+  //       if (0 < n.day() && n.day() < 6) {
+  //         events.push({
+  //           start: n.toDate(),
+  //           end: n.add(parseInt(value.Duration), "s").toDate(),
+  //           title: value.Title,
+  //           allDay: value.fAllDayEvent === true
+  //         });
+  //       }
+  //     }
+  //   } else if (rule.$dayFrequency) {
+  //     for (
+  //       let m: moment.Moment = moment(value.EventDate);
+  //       m.isBefore(moment(value.EndDate));
+  //       m.add(rule.$dayFrequency, "d")
+  //     ) {
+  //       let n: moment.Moment = moment(m);
+  //       events.push({
+  //         start: n.toDate(),
+  //         end: n.add(parseInt(value.Duration), "s").toDate(),
+  //         title: value.Title,
+  //         allDay: value.fAllDayEvent === true
+  //       });
+  //     }
+  //   }
 
-    return events;
-  }
+  //   return events;
+  // }
 
-  private static parseMonthly(value: SPEvent): RBCEvent[] {
-    return [];
-  }
+  // private static parseMonthly(value: SPEvent): RBCEvent[] {
+  //   return [];
+  // }
 }
